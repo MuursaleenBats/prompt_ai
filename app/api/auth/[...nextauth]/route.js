7cb12667edc4477f9f32f2abb6783fb64b9572c8
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-08-21 22:06:25 +0530

    databse connection

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
+import { connectToDb } from "@utils/database";
 
 console.log({
     clientId: process.env.GOOGLE_ID,
@@ -17,7 +18,18 @@ const handler = NextAuth({
 
     },
     async signIn({profile}){
+        try {
+            await connectToDb();
 
+            //check if a user already exists
+
+            //if user does not exist, create new user
+
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
     }
 })
 
