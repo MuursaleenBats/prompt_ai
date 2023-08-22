7d36d1fa4a38b15524452038f2c72c08e8cfe5e2
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-08-22 23:11:07 +0530

    textarea added

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link';
 
 const Form = ({
   type,
@@ -8,7 +9,35 @@ const Form = ({
   handleSubmit
 }) => {
   return (
-    <div>Form</div>
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text'>
+        <span className='orange_gradient'>
+          {type} Post
+        </span>
+      </h1>
+      <p className='desc max-w-md'>
+        {type} and share prompts with the world, and let your imagination run wild with any AI-Powered platform
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className='max-w-2xl w-full mt-10 flex flex-col gap 7 glassmorphism'
+      >
+        <span className='font-satoshi text-base text-gray-700 font-semibold'>
+          Your AI Prompt
+        </span>
+
+        <textarea 
+          value={post.prompt}
+          onChange={(e) => setPost({...post,
+              prompt: e.target.value
+            })
+          }
+          placeholder='Write Your Prompt Here...'
+          className='form_textarea'
+        />
+      </form>
+    </section>
   )
 }
 
