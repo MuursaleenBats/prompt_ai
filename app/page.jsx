919617faec643d8fec3c695c25e34c626fe28666
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-08-27 18:03:44 +0530

    Feed showing only when signed in

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,9 @@
+'use client'
 import Feed from '@components/Feed'
+import { useSession } from 'next-auth/react';
 
 const Home = () => {
+  const {data: session} = useSession();
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
@@ -13,7 +16,7 @@ const Home = () => {
         PromptAI is an open-source AI prompting tool for modern world to discover, create and share creative prompts 
     </p>
 
-    <Feed />
+    {session?.user ? <Feed /> : <></>}
     </section>
   );
 };
