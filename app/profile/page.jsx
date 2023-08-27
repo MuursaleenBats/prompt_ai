37501c9672744e3587abc6a3c1125a42db8f99aa
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-08-27 20:00:58 +0530

    update and delete implemented on frontend

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -28,8 +28,22 @@ const MyProfile = () => {
       }
     }
 
-    const handleDelete = () => {
+    const handleDelete = async (post) => {
+      const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
 
+      if(hasConfirmed){
+        try {
+          await fetch(`/api/prompt/${post._id}`,{
+            method: "DELETE"
+          });
+
+          const newPosts = posts.filter((p) => p._id !== post._id);
+
+          setPosts(newPosts);
+        } catch (error) {
+          console.log(error);
+        }
+      }
     }
 
 
